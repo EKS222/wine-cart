3e9e39a4f8e020ebace39de7833c9fb86125bb33
EKS222 <stainlesssssssssss@gmail.com>
2024-10-25 10:43:24 +0300

    Update routes.py
    Added And delete unnecessary and necessary routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,8 +12,6 @@ bcrypt = Bcrypt()
 logging.basicConfig(level=logging.DEBUG)
 
 
-
-
 @routes.route('/')
 def home():
     data = {"message": "wine-app-api"}
@@ -96,51 +94,6 @@ def patch_user(user_id):
     else:
         return jsonify({'message': 'User not found'})
 
-
-@routes.route('/wine_types', methods=['GET'])
-def get_all_wine_types():
-    wine_types = WineTypes.query.all()
-    serialized_wine_types = [{'type_id': wine_type.type_id, 'type_name': wine_type.type_name} for wine_type in wine_types]
-    return jsonify({'wine_types': serialized_wine_types})
-
-# Route to create a new WineType
-@routes.route('/wine_types', methods=['POST'])
-def create_wine_type():
-    wine_type_data = request.get_json()
-    new_wine_type = WineTypes(type_name=wine_type_data.get('type_name'))
-    db.session.add(new_wine_type)
-    db.session.commit()
-    return jsonify({'message': 'WineType created successfully', 'type_id': new_wine_type.type_id})
-
-# Similar routes for Regions
-@routes.route('/regions', methods=['GET'])
-def get_all_regions():
-    regions = Regions.query.all()
-    serialized_regions = [{'region_id': region.region_id, 'region_name': region.region_name} for region in regions]
-    return jsonify({'regions': serialized_regions})
-
-@routes.route('/regions', methods=['POST'])
-def create_region():
-    region_data = request.get_json()
-    new_region = Regions(region_name=region_data.get('region_name'))
-    db.session.add(new_region)
-    db.session.commit()
-    return jsonify({'message': 'Region created successfully', 'region_id': new_region.region_id})
-
-# Similar routes for Varietals
-@routes.route('/varietals', methods=['GET'])
-def get_all_varietals():
-    varietals = Varietals.query.all()
-    serialized_varietals = [{'varietal_id': varietal.varietal_id, 'varietal_name': varietal.varietal_name, 'region_name': varietal.region_name} for varietal in varietals]
-    return jsonify({'varietals': serialized_varietals})
-
-@routes.route('/varietals', methods=['POST'])
-def create_varietal():
-    varietal_data = request.get_json()
-    new_varietal = Varietals(varietal_name=varietal_data.get('varietal_name'), region_name=varietal_data.get('region_name'))
-    db.session.add(new_varietal)
-    db.session.commit()
-    return jsonify({'message': 'Varietal created successfully', 'varietal_id': new_varietal.varietal_id})
 
 # Similar routes for Wines
 @routes.route('/wines', methods=['GET'])
